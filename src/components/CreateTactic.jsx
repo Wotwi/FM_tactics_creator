72f5d6c0483fdb66d6c95083e6a249cd7d258fa0
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-08-09 19:24:51 +0200

    added handle for select and generating things on pitch

--- src/components/CreateTactic.jsx
+++ src/components/CreateTactic.jsx
@@ -1,14 +1,37 @@
+import {useState} from "react";
+
 function CreateTactic() {
+    const [selectedFormation, setSelectedFormation] = useState('');
+
+    function handleSelectFormation (e) {
+        setSelectedFormation(e.target.value)
+
+        if (e.target.value === "3-5-2") {
+            console.log('wybrałeś 3-5-2');
+            generateElement()
+        } else if (e.target.value === "4-3-3") {
+            console.log('wybrałeś 4-3-3');
+            generateElement()
+        }
+    }
+
+    function generateElement () {
+        return <h2 className="generator">{selectedFormation}</h2>
+    }
+
+
+
     return (
         <>
             <div className="container">
                 <span className="tactic__wrapper">
                     <label htmlFor="formations" className="formation">Choose your formation:</label>
-                    <select name="formations" className="formation-select" id="formations">
-                        <option className="formation-select__item" value="3-5-2">3-5-2</option>
-                        <option className="formation-select__item" value="4-2-2-2 DP Wąsko">4-2-2-2 DP Wąsko</option>
-                        <option className="formation-select__item" value="4-2-3-1">4-2-3-1</option>
-                        <option className="formation-select__item" value="4-3-3">4-3-3</option>
+                    <select value={selectedFormation} onChange={handleSelectFormation} name="formations" className="formation-select" id="formations">
+                        <option id="option1" className="formation-select__item"></option>
+                        <option id="option2" className="formation-select__item" value="3-5-2">3-5-2</option>
+                        <option id="option3" className="formation-select__item" value="4-2-2-2 DP Wąsko">4-2-2-2 DP Wąsko</option>
+                        <option id="option4" className="formation-select__item" value="4-2-3-1">4-2-3-1</option>
+                        <option id="option5" className="formation-select__item" value="4-3-3">4-3-3</option>
                     </select>
                     <main className="tactics">
                         <section className="options">
@@ -26,8 +49,8 @@ function CreateTactic() {
                             <div className="in-transition option-font">IN TRANSITION</div>
                             <div className="out-of-possession option-font">OUT OF POSSESSION</div>
                         </section>
-                        <section className="pitch">
-
+                        <section id="pitch" className="pitch">
+                            {generateElement()}
                         </section>
                     </main>
                     <button className="btn">Anuluj</button>
