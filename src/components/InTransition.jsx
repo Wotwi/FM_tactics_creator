8170a84229e25f54b8f9414b530583504ca4fb2c
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-08-30 19:40:47 +0200

    added handle to all buttons in InTransition element

--- src/components/InTransition.jsx
+++ src/components/InTransition.jsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function InTransition({showInTransition, closeInTransition}) {
+
+    const [counterPress, setCounterPress] = useState(false);
+    const [regroup, setRegroup] = useState(false);
+
+    const [counter, setCounter] = useState(false);
+    const [holdShape, setHoldShape] = useState(false);
+
+    const [distributeQuickly, setDistributeQuickly] = useState(false);
+    const [slowPaceDown, setSlowPaceDown] = useState(false);
+
+    const [distributionType, setDistributionType] = useState({
+        rollItOut: false,
+        throwItLong: false,
+        takeShortKicks: false,
+        takeLongKicks: false,
+    })
+
+    const handleCounterPress = () => {
+        setCounterPress(!counterPress);
+        setRegroup(false);
+    }
+
+    const handleRegroup = () => {
+        setRegroup(!regroup);
+        setCounterPress(false);
+    }
+
+    const handleCounter = () => {
+        setCounter(!counter);
+        setHoldShape(false);
+    }
+
+    const handleHoldShape = () => {
+        setHoldShape(!holdShape);
+        setCounter(false);
+    }
+
+    const handleDistributeQuickly = () => {
+        setDistributeQuickly(!distributeQuickly);
+        setSlowPaceDown(false);
+    }
+
+    const handleSlowPaceDown = () => {
+        setSlowPaceDown(!slowPaceDown);
+        setDistributeQuickly(false);
+    }
+
+    const handleDistributionType = (clickedOption) => {
+        setDistributionType((prevState) => ({
+            rollItOut: (clickedOption === 'rollItOut' ? !prevState.rollItOut : false),
+            throwItLong: (clickedOption === 'throwItLong' ? !prevState.throwItLong : false),
+            takeShortKicks: (clickedOption === 'takeShortKicks' ? !prevState.takeShortKicks : false),
+            takeLongKicks: (clickedOption === 'takeLongKicks' ? !prevState.takeLongKicks : false),
+        }));
+    };
+
     return (
         <>
             {showInTransition && <div className="commands-window">
                 <div className="options-section">
                     <h3 className="options-title">WHEN POSSESSION HAS BEEN LOST</h3>
-                    <button className="options-btn final-third-btn">Counter-Press</button>
-                    <button className="options-btn final-third-btn">Regroup</button>
+                    <button className={counterPress ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleCounterPress}>Counter-Press</button>
+                    <button className={regroup ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleRegroup}>Regroup</button>
                 </div>
                 <div className="options-section">
                     <h3 className="options-title">WHEN POSSESSION HAS BEEN WON</h3>
-                    <button className="options-btn final-third-btn">Counter</button>
-                    <button className="options-btn final-third-btn">Hold Shape</button>
+                    <button className={counter ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleCounter}>Counter</button>
+                    <button className={holdShape ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleHoldShape}>Hold Shape</button>
                 </div>
                 <div className="options-section">
                     <h3 className="options-title">GOALKEEPER IN POSSESSION</h3>
-                    <button className="options-btn final-third-btn">Distribute Quickly</button>
-                    <button className="options-btn final-third-btn">Slow Pace Down</button>
+                    <button className={distributeQuickly ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleDistributeQuickly}>Distribute Quickly</button>
+                    <button className={slowPaceDown ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={handleSlowPaceDown}>Slow Pace Down</button>
 
                     <section className="small-pitch">
                         <span className="oop-pitch-element">Distribute Over Opposition Defence</span>
@@ -33,10 +89,10 @@ function InTransition({showInTransition, closeInTransition}) {
                     </section>
 
                     <h3 className="options-title">DISTRIBUTION TYPE</h3>
-                    <button className="options-btn final-third-btn">Roll It Out</button>
-                    <button className="options-btn final-third-btn">Throw It Long</button>
-                    <button className="options-btn final-third-btn">Take Short Kicks</button>
-                    <button className="options-btn final-third-btn">Take Long Kicks</button>
+                    <button className={distributionType.rollItOut ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={() => handleDistributionType('rollItOut')}>Roll It Out</button>
+                    <button className={distributionType.throwItLong ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={() => handleDistributionType('throwItLong')}>Throw It Long</button>
+                    <button className={distributionType.takeShortKicks ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={() => handleDistributionType('takeShortKicks')}>Take Short Kicks</button>
+                    <button className={distributionType.takeLongKicks ? "options-btn-active final-third-btn" : "options-btn final-third-btn"} onClick={() => handleDistributionType('takeLongKicks')}>Take Long Kicks</button>
                 </div>
 
                 <button className="close-btn" onClick={closeInTransition}>X</button>
