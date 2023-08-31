c95f98408e10dd74b0e2ebae4c122eceabb9c599
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-08-31 20:03:20 +0200

    added arrows to defensive shape screen

--- src/components/OutOfPossession.jsx
+++ src/components/OutOfPossession.jsx
@@ -1,11 +1,63 @@
+import {createElement, useState} from "react";
+
 function OutOfPossession({showOutOfPossession, defensiveWidthRange, triggerPressRange, closeOutOfPossession, handleDefensiveWidth, handleTriggerPress}) {
+
+    const [tighterMarking, setTighterMarking] = useState(false);
+
+    const [preventShortGK, setPreventShortGK] = useState(false);
+
+    const [stayOnFeet, setStayOnFeet] = useState(false);
+    const [getStuckIn, setGetStuckIn] = useState(false);
+
+    const handleTighterMarking = () => {
+        setTighterMarking(!tighterMarking);
+    }
+
+    const handlePreventShortGK = () => {
+        setPreventShortGK(!preventShortGK);
+    }
+
+    const handleStayOnFeet = () => {
+        setStayOnFeet(!stayOnFeet);
+        setGetStuckIn(false);
+    }
+
+    const handleGetOnStuck = () => {
+        setGetStuckIn(!getStuckIn);
+        setStayOnFeet(false);
+    }
+
+    const generateArrows = () => {
+        const arrows = [];
+
+        if (defensiveWidthRange === 0) {
+            for (let i = 0; i <= 5; i++) {
+                const defensiveShape = createElement(
+                    'img',
+                    {className: `arrow-outside${i}`, src: "src/assets/arrow.png"}
+                )
+                arrows.push(defensiveShape);
+            }
+        } else if (defensiveWidthRange === 2) {
+            for (let i = 0; i <= 5; i++) {
+                const defensiveShape = createElement(
+                    'img',
+                    {className: `arrow-inside${i}`, src: "src/assets/arrow.png"}
+                )
+                arrows.push(defensiveShape);
+            }
+        }
+        return arrows;
+    }
+
     return (
         <>
             {showOutOfPossession && <div className="commands-window">
                 <div className="options-section oop-section">
                     <h3 className="options-title">DEFENSIVE SHAPE</h3>
                     <section className="small-pitch oop-pitch">
-
+                        {defensiveWidthRange === 0 && generateArrows()}
+                        {defensiveWidthRange === 2 && generateArrows()}
                     </section>
 
                     <h3 className="options-title">DEFENSIVE WIDTH</h3>
@@ -18,7 +70,7 @@ function OutOfPossession({showOutOfPossession, defensiveWidthRange, triggerPress
                 </div>
                 <div className="options-section oop-section">
                     <h3 className="options-title">MARKING AND TACKLING</h3>
-                    <button className="options-btn final-third-btn">Use Tighter Marking</button>
+                    <button className={tighterMarking ? 'options-btn-active final-third-btn' : 'options-btn final-third-btn'} onClick={handleTighterMarking}>Use Tighter Marking</button>
 
                     <h3 className="options-title">TRIGGER PRESS</h3>
                     <p className="options-chosen">
@@ -29,11 +81,11 @@ function OutOfPossession({showOutOfPossession, defensiveWidthRange, triggerPress
                         {triggerPressRange === 4 ? "Much More Often" : ""}
                     </p>
                     <input type="range" className="range-input" min="0" max="4" value={triggerPressRange} onChange={handleTriggerPress}/>
-                    <button className="options-btn final-third-btn">Prevent Short GK Distribu...</button>
+                    <button className={preventShortGK ? 'options-btn-active final-third-btn' : 'options-btn final-third-btn'} onClick={handlePreventShortGK}>Prevent Short GK Distribu...</button>
 
                     <h3 className="options-title">TACKLING</h3>
-                    <button className="options-btn final-third-btn">Stay On Feet</button>
-                    <button className="options-btn final-third-btn">Get Stuck It</button>
+                    <button className={stayOnFeet ? 'options-btn-active final-third-btn' : 'options-btn final-third-btn'} onClick={handleStayOnFeet}>Stay On Feet</button>
+                    <button className={getStuckIn ? 'options-btn-active final-third-btn' : 'options-btn final-third-btn'} onClick={handleGetOnStuck}>Get Stuck It</button>
                 </div>
                 <button className="close-btn" onClick={closeOutOfPossession}>X</button>
             </div>}
