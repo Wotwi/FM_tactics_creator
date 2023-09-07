a6e0e808e48baa0759d1c33692bf7c80658bae5c
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-07 16:23:47 +0200

    added content to hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Header from "./Header.jsx";
 
 function Hero(props) {
     return (
         <>
-            
+            <section className="container hero__wrapper">
+                <h1 className="hero__header">Hello in FM Tactics Creator</h1>
+                <p className="hero__content">"FM Tactics Creator" is a tool that allows you to design and improve your tactics for Football Manager. Create and share your own tactics with others.</p>
+                <p className="hero__content">Work on completing the application is still ongoing.</p>
+            </section>
         </>
     );
 }
