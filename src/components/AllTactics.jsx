50b929b813b00f00d84a60ba8cf30a9a9dc475d7
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-07 12:26:52 +0200

    added new components to all tactics and home page

---
+++ src/components/AllTactics.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AllTactics(props) {
+    return (
+        <>
+            <article>
+
+            </article>
+        </>
+    );
+}
+
+export default AllTactics;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "./Header.jsx";
+
+function Hero(props) {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Hero;
