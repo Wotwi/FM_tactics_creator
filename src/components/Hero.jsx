7507480663bae8a395b09f6b472a0ace488230ad
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-08 12:21:20 +0200

    added info about features to hero page and styled them

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,13 @@ function Hero(props) {
                 <h1 className="hero__header">Hello in FM Tactics Creator</h1>
                 <p className="hero__content">"FM Tactics Creator" is a tool that allows you to design and improve your tactics for Football Manager. Create and share your own tactics with others.</p>
                 <p className="hero__content">Work on completing the application is still ongoing.</p>
+                <h5 className="features">Upcoming features:</h5>
+                <ul className="features-ul">
+                    <li className="features-element">Log in/log out</li>
+                    <li className="features-element">Saving tactics on your profile</li>
+                    <li className="features-element">Sharing your tactics with others</li>
+                    <li className="features-element">Viewing other users tactics</li>
+                </ul>
             </section>
         </>
     );
