2a88d6c24892ca53598b768f314ee6147cab32e5
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-12 17:52:49 +0200

    added register function

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,29 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, {useState, useEffect} from 'react';
+import { auth } from "../../firebase-config.js"
 
 function Register(props) {
 
+    const [registerEmail, setRegisterEmail] = useState("")
+    const [registerPassword, setRegisterPassword] = useState("")
+
+    const register = async (e) => {
+        e.preventDefault();
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+            console.log(user)
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <>
-            <form>
+            <form className='register__form'>
                 <h3>Register</h3>
-                <input type="text" placeholder='Email...' />
-                <input type="password" placeholder='Password...' />
+                <input type="text" placeholder='Email...' onChange={(e) => {setRegisterEmail(e.target.value)}} />
+                <input type="password" placeholder='Password...' onChange={(e) => {setRegisterPassword(e.target.value)}} />
+                <input type="submit" onClick={register} value="Zarejestruj"/>
             </form>
         </>
     );
