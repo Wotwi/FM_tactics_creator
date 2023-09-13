19ce37145d794f3b3b8f37f58ce599a8e2960b09
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-13 13:37:52 +0200

    added displaying logged users email in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { auth } from '../../firebase-config';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react';
 
 function Header() {
+
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        })
+    }, [])
+
     return (
         <>
             <header className="header">
@@ -12,8 +24,15 @@ function Header() {
                         <a href="/alltactics" className="nav__element">All tactics</a>
                         <p className="nav__element">Your tactics</p>
                         <span className="account">
-                            <a href="/login" className="nav__element">Login</a>
-                            <a href="/register" className="nav__element">Register</a>
+                            {user ? (
+                                <p className='nav__element'>Hello {user.email}!</p>
+                            ) : (
+                                <div>
+                                    <a href="/login" className="nav__element">Login</a>
+                                    <a href="/register" className="nav__element">Register</a>
+                                </div>
+                            )}
+                            
                         </span>
                     </span>
                 </nav>
