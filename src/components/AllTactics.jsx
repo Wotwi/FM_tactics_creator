45f0c76e84a422da025bcd96616939c54ad3006b
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-09-18 20:13:05 +0200

    added useEffect to fetch all tactics from json server

--- src/components/AllTactics.jsx
+++ src/components/AllTactics.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function AllTactics(props) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/tactics')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error('Error', error))
+    }, [])
+
     return (
         <>
-            <article>
-
-            </article>
+            {data.map(item => (
+                <div className='all__tactics__wrapper'>
+                    <article className='single__tactic' key={item.key}>
+                        <p className='tactic__formation'>{item.formation}</p>
+                        <span>
+                            <h4 className='tactic__title'>{item.title}</h4>
+                            <p className='tactic__body'>{item.body}</p>
+                        </span>
+                    </article>
+                </div>
+            ))}
         </>
-    );
+    )
 }
 
 export default AllTactics;
