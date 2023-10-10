a29f3aab9113759229d4a58f0a321be2298f6d98
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-10-10 16:18:36 +0200

    added handle to hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ function Header() {
 
     const [user, setUser] = useState("");
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [hamburgerActive, setHamburgerActive] = useState(false);
 
     useEffect(() => {
         onAuthStateChanged(auth, (currentUser) => {
@@ -29,13 +30,76 @@ function Header() {
         await signOut(auth)
     }
 
+    const hamburgerClick = () => {
+        setHamburgerActive(!hamburgerActive);
+    }
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (hamburgerActive && !event.target.closest('.your-menu-class')) {
+                setHamburgerActive(false);
+            }
+        };
+    
+        document.addEventListener('click', handleOutsideClick);
+    
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, [hamburgerActive]);
+
     return (
         <>
             <header className="header">
                 <nav className="main__navigation">
                     <a className="logo" href="/">FM Tactics Creator</a>
                     {windowWidth < 420 ? (
-                            <img src={hamburger} className='hamburger'></img>
+                            <>
+                                <img src={hamburger} onClick={hamburgerClick} className='hamburger'></img>
+                                {hamburgerActive ? (
+                                    <span>
+                                  
+                                        <span className="elements__wrapper">
+                                            {user ? (
+                                                <>
+                                                    <a href="/create" className="nav__element">Create tactic</a>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <a href='/login' className="nav__element">Create tactic</a>
+                                                </>
+                                            )}
+                                            <a href="/alltactics" className="nav__element">All tactics</a>
+                                            {user ? (
+                                                <>
+                                                    <a className="nav__element" href="/yourtactics">Your tactics</a>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <a href='/login' className="nav__element">Your tactics</a>
+                                                </>
+                                            )}
+                                            <span className="account">
+                                                {user ? (
+                                                    <>
+                                                        <p className='nav__element'>Hello {user.email}!</p>
+                                                        <button className='logout__button' onClick={logout}>Log Out</button>
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <a href="/login" className="nav__element">Login</a>
+                                                        <a href="/register" className="nav__element">Register</a>
+                                                    </>
+                                            )}
+                                        </span>
+
+                                    </span>                            
+
+                                    </span>
+                                ) : (
+                                    <></>
+                                )}
+                            </>
                         ) : (
                             <span className="elements__wrapper">
                                 {user ? (
